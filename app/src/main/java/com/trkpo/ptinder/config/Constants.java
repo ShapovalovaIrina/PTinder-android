cea863f7168465c42677a810a66491834e77f158
AnBrusn <brusnitsyna.an@yandex.ru>
2020-12-21 17:29:11 +0300

    Add notification fragment

--- app/src/main/java/com/trkpo/ptinder/config/Constants.java
+++ app/src/main/java/com/trkpo/ptinder/config/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
     public static final String SEARCH_PATH = SERVER_PATH + "/search";
     public static final String NEWS_PATH = SERVER_PATH + "/news";
     public static final String FAVOURITE_PATH = SERVER_PATH + "/favourites";
+    public static final String NOTIFICATIONS_PATH = SERVER_PATH + "/notifications";
 
     public static String USER_ICON_URL;
 }
