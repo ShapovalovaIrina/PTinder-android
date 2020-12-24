55ac69f1f2301be728d0617a2c93acf224b0037a
AnBrusn <brusnitsyna.an@yandex.ru>
2020-12-24 12:32:21 +0300

    Add help page

---
+++ app/src/main/java/com/trkpo/ptinder/ui/HelpFragment.java
@@ -0,0 +1,26 @@
+package com.trkpo.ptinder.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.trkpo.ptinder.R;
+
+public class HelpFragment extends Fragment {
+
+    private Activity activity;
+    private View root;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_help, container, false);
+        activity = getActivity();
+
+        return root;
+    }
+}
