ef234b10be7d2084a6a45f5b4f589597a698e9e3
Shapovalova Irina <irishapovalova13@gmail.com>
2021-03-06 00:13:09 +0300

    pojo unit test

---
+++ app/src/test/java/com/trkpo/ptinder/pojo/GenderTest.java
@@ -0,0 +1,12 @@
+package com.trkpo.ptinder.pojo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GenderTest {
+    @Test
+    public void genderEnum() {
+        Gender gender = Gender.FEMALE;
+    }
+}
