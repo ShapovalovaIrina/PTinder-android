c611c5a39d0c72718e1051779a4d1d965a5f5e91
Shapovalova Irina <irishapovalova13@gmail.com>
2021-03-29 22:46:34 +0300

    Comment wait for emulator script

--- docker/ui_test_on_emulator.sh
+++ docker/ui_test_on_emulator.sh
@@ -5,25 +5,27 @@ set -eu
 #
 # Wait for emulator
 #
-bootanim=""
-failcounter=0
-timeout_in_sec=360
+#bootanim=""
+#failcounter=0
+#timeout_in_sec=360
+#
+#until [[ "$bootanim" =~ "stopped" ]]; do
+#  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+#  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
+#    || "$bootanim" =~ "running" ]]; then
+#    let "failcounter += 1"
+#    echo "Waiting for emulator to start"
+#    if [[ $failcounter -gt timeout_in_sec ]]; then
+#      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+#      exit 1
+#    fi
+#  fi
+#  sleep 1
+#done
 
-until [[ "$bootanim" =~ "stopped" ]]; do
-  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
-  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
-    || "$bootanim" =~ "running" ]]; then
-    let "failcounter += 1"
-    echo "Waiting for emulator to start"
-    if [[ $failcounter -gt timeout_in_sec ]]; then
-      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
-      exit 1
-    fi
-  fi
-  sleep 1
-done
+sleep 60
 
-echo "Emulator is ready"
+echo "Emulator \"is ready\""
 
 #
 # Unlock emulator screen
