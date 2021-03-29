580386dfd451ca3e33f2a0da359ce58dbb84ce7d
Shapovalova Irina <irishapovalova13@gmail.com>
2021-03-29 18:28:27 +0300

    UI tests

--- docker/docker.sh
+++ docker/docker.sh
@@ -5,4 +5,4 @@ docker stack deploy -c docker/docker-compose.yml app
 echo "Wait 30 seconds for container to start up"
 sleep 30
 docker stack ps app
-curl localhost:8080/ptinder/pets/types
+#curl localhost:8080/ptinder/pets/types
